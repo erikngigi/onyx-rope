80601353e195392e9c5f133c5a063e506a7fb51c
erikngigi <ericmosesngigi@gmail.com>
2026-05-14 11:59:16 +0300

    fix(android): repair build.gradle.kts syntax and migrate kotlinOptions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID
@@ -26,7 +28,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 36
-        versionCode = flutter.versionCode
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
